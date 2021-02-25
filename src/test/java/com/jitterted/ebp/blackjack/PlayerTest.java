d4c3934872527f76bf0fabbc075ada8ee8078b24
Ken Lewis <kenneth.lewis@salesforce.com>
2021-02-24 20:51:34 -0800

    Add new tests for Player class

---
+++ src/test/java/com/jitterted/ebp/blackjack/PlayerTest.java
@@ -0,0 +1,63 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PlayerTest {
+
+    @Test
+    public void newPlayerNoDepositsBalanceIs0() {
+        Player player = new Player();
+
+        assertThat(player.balance())
+                .isEqualTo(0);
+    }
+
+    @Test
+    public void playerWithBalanceOf100Bets40BalanceIs60() {
+        Player player = new Player();
+        player.deposits(100);
+
+        player.bets(40);
+
+        assertThat(player.balance())
+                .isEqualTo(60);
+    }
+
+    @Test
+    public void playerWithBalanceOf100Bets40AndWinsBalanceIs140() {
+        Player player = new Player();
+        player.deposits(100);
+
+        player.bets(40);
+        player.wins();
+
+        assertThat(player.balance())
+                .isEqualTo(140);
+    }
+
+    @Test
+    public void playerWithBalanceOf150Bets40AndLosesBalanceIs110() {
+        Player player = new Player();
+        player.deposits(150);
+
+        player.bets(40);
+        player.loses();
+
+        assertThat(player.balance())
+                .isEqualTo(110);
+    }
+
+    @Test
+    public void playerWithBalanceOf150Bets40AndTiesBalanceIs150() {
+        Player player = new Player();
+        player.deposits(150);
+
+        player.bets(40);
+        player.ties();
+
+        assertThat(player.balance())
+                .isEqualTo(150);
+    }
+}
